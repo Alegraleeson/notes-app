f81964547844d7e3457de72d3e26abce1704d0fb
Alegraleeson <wingmonkee@yahoo.com>
2022-09-09 16:42:04 -0500

    fixed the error from the fix

--- starter/src/main/java/com/devmountain/noteApp/controllers/UserController.java
+++ starter/src/main/java/com/devmountain/noteApp/controllers/UserController.java
@@ -19,7 +19,7 @@ public class UserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @PostMapping("/register.css")
+    @PostMapping("/register")
     public List<String> addUser(@RequestBody UserDto userDto){
         String passHash = passwordEncoder.encode(userDto.getPassword());
         userDto.setPassword(passHash);
